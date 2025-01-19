07c6703ff98beb687c9785f964913780d0118fdc
x-dr <xd9722@gmail.com>
2025-01-20 02:18:20 +0800

    a

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-#===============================================
-# File name: init-settings.sh
-# Description: This script will be executed during the first boot
-# Author: SuLingGG
-# Blog: https://mlapp.cn
-#===============================================
 
 # Set default theme to luci-theme-argon
 uci set luci.main.mediaurlbase='/luci-static/argon'
 uci commit luci
-
-# Disable opkg signature check
-sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
 
 # Disable IPV6 ula prefix
 # sed -i 's/^[^#].*option ula/#&/' /etc/config/network
@@ -19,5 +10,10 @@ sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
 # Check file system during boot
 # uci set fstab.@global[0].check_fs=1
 # uci commit fstab
+
+# Set etc/openwrt_release
+sed -i "s/DISTRIB_REVISION=.*/DISTRIB_REVISION=''/g" /etc/openwrt_release
+repo=$(cat /etc/openwrt_release | grep DISTRIB_DESCRIPTION= | awk -F "'" '{print $2}' | awk '{print $1}')
+sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='$repo R$(date +%y.%m.%d)'/g" /etc/openwrt_release
 
 exit 0
--- scripts/preset-terminal-tools.sh
+++ scripts/preset-terminal-tools.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
-#===============================================
-# File name: preset-terminal-tools.sh
-# System Required: Linux
-# Version: 1.0
-# Author: SuLingGG
-# Blog: https://mlapp.cn
-#===============================================
 
-mkdir -p files/root
-pushd files/root
+[ -d files/root ] || mkdir -p files/root
 
-# Install oh-my-zsh
 # Clone oh-my-zsh repository
-git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh ./.oh-my-zsh
+git clone -q https://github.com/ohmyzsh/ohmyzsh files/root/.oh-my-zsh
 
 # Install extra plugins
-git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
+git clone -q https://github.com/zsh-users/zsh-autosuggestions files/root/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone -q https://github.com/zsh-users/zsh-syntax-highlighting files/root/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone -q https://github.com/zsh-users/zsh-completions files/root/.oh-my-zsh/custom/plugins/zsh-completions
 
 # Get .zshrc dotfile
-cp $GITHUB_WORKSPACE/scripts/.zshrc .
-
-popd
+cp $GITHUB_WORKSPACE/scripts/.zshrc files/root
