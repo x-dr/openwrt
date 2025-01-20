79014dd3b62c0265e175d77b976809c308ffc859
x-dr <xd9722@gmail.com>
2025-01-20 14:52:05 +0800

    s

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set default theme to luci-theme-argon
-uci set luci.main.mediaurlbase='/luci-static/argon'
-uci commit luci
+# uci set luci.main.mediaurlbase='/luci-static/argon'
+# uci commit luci
 
 # Disable IPV6 ula prefix
 # sed -i 's/^[^#].*option ula/#&/' /etc/config/network
@@ -12,8 +12,8 @@ uci commit luci
 # uci commit fstab
 
 # Set etc/openwrt_release
-sed -i "s/DISTRIB_REVISION=.*/DISTRIB_REVISION=''/g" /etc/openwrt_release
-repo=$(cat /etc/openwrt_release | grep DISTRIB_DESCRIPTION= | awk -F "'" '{print $2}' | awk '{print $1}')
-sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='$repo R$(date +%y.%m.%d)'/g" /etc/openwrt_release
+# sed -i "s/DISTRIB_REVISION=.*/DISTRIB_REVISION=''/g" /etc/openwrt_release
+# repo=$(cat /etc/openwrt_release | grep DISTRIB_DESCRIPTION= | awk -F "'" '{print $2}' | awk '{print $1}')
+# sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='$repo R$(date +%y.%m.%d)'/g" /etc/openwrt_release
 
-exit 0
+# exit 0
